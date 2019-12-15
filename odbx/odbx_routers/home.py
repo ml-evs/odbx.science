27109d5608005ba5be0bf753bf966c83c673b780
Matthew Evans <me388@cam.ac.uk>
2019-12-15 21:21:18 +0000

    Added home and single structure routers, with dummy cif for now

---
+++ odbx/odbx_routers/home.py
@@ -0,0 +1,34 @@
+""" odbx version of optimade.server.routers.structures.py, including templates. """
+
+from typing import Union
+
+from fastapi import APIRouter, Depends
+from starlette.requests import Request
+
+from optimade.models import (
+    ErrorResponse,
+    StructureResource,
+    StructureResponseMany,
+    StructureResponseOne,
+)
+from optimade.server.config import CONFIG
+from optimade.server.deps import EntryListingQueryParams, SingleEntryQueryParams
+from optimade.server.entry_collections import MongoCollection, client
+from optimade.server.mappers import StructureMapper
+
+from optimade.server.routers.utils import get_entries, get_single_entry
+from odbx_templates import TEMPLATES
+
+router = APIRouter()
+
+
+@router.get("/")
+def homepage(request: Request):
+    context = {
+        "request": request,
+        "odbx_title": "odbx",
+        "odbx_blurb": "the open database of xtals",
+        "odbx_about": 'odbx is a public database of crystal structures from the group of <a href="https://ajm143.github.io">Dr Andrew Morris</a> at the University of Birmingham.',
+    }
+
+    return TEMPLATES.TemplateResponse("home.html", context)
---
+++ odbx/odbx_routers/structures.py
@@ -0,0 +1,74 @@
+""" odbx version of optimade.server.routers.structures.py, including templates. """
+
+from typing import Union
+from pathlib import Path
+
+from fastapi import APIRouter, Depends
+from starlette.requests import Request
+
+from optimade.models import (
+    ErrorResponse,
+    StructureResource,
+    StructureResponseOne,
+    EntryResponseOne,
+)
+from optimade.server.config import CONFIG
+from optimade.server.deps import SingleEntryQueryParams
+from optimade.server.entry_collections import MongoCollection, client
+
+from optimade.server.routers.utils import get_single_entry
+from odbx_mappers import StructureMapper
+from odbx_templates import TEMPLATES
+
+router = APIRouter()
+
+
+with open(Path(__file__).resolve().parent.joinpath("test.cif"), "r",) as f:
+    cif_string = "".join(
+        [line for line in f.readlines() if not line.strip().startswith("#")]
+    ).replace("'", "\\'")
+
+structures_coll = MongoCollection(
+    collection=client[CONFIG.mongo_database][CONFIG.structures_collection],
+    resource_cls=StructureResource,
+    resource_mapper=StructureMapper,
+)
+
+
+@router.get(
+    "/structures/{entry_id:path}",
+    response_model=Union[StructureResponseOne, ErrorResponse],
+    response_model_exclude_unset=True,
+    tags=["Structure"],
+)
+def get_single_structure(
+    request: Request, entry_id: str, params: SingleEntryQueryParams = Depends()
+):
+
+    response = get_single_entry(
+        collection=structures_coll,
+        entry_id=entry_id,
+        request=request,
+        params=params,
+        response=EntryResponseOne,
+    )
+
+    context = {
+        "request": request,
+        "entry_id": entry_id,
+    }
+
+    if response.meta.data_returned < 1:
+        return TEMPLATES.TemplateResponse("structure_not_found.html", context)
+
+    context.update(
+        {
+            "odbx_title": "odbx",
+            "odbx_blurb": "the open database of xtals",
+            "odbx_about": 'odbx is a public database of crystal structures from the group of <a href="https://ajm143.github.io">Dr Andrew Morris</a> at the University of Birmingham.',
+            "odbx_cif_string": cif_string,
+            "structure_info": response,
+        }
+    )
+
+    return TEMPLATES.TemplateResponse("structure.html", context)

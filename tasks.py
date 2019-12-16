c3996498946a48b04ab2c85bbaf038479ddb70a7
Matthew Evans <me388@cam.ac.uk>
2019-12-16 13:36:02 +0000

    Updated jslib task

--- tasks.py
+++ tasks.py
@@ -1,12 +1,14 @@
 import os
+from pathlib import Path
 from invoke import task
 
 
 @task
 def get_js_libs(c):
     old_dir = os.getcwd()
-    os.makedirs("odbx/js/lib", exist_ok=True)
-    os.chdir("odbx/js/lib")
+    lib_path = Path(__file__).parent.joinpath("odbx/js/lib")
+    os.makedirs(lib_path, exist_ok=True)
+    os.chdir(lib_path)
     c.run("wget https://web.chemdoodle.com/downloads/ChemDoodleWeb-8.0.0.zip")
     c.run("unzip ChemDoodleWeb-8.0.0.zip")
     os.chdir(old_dir)

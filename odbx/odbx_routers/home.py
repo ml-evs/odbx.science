a6242d94d9079a65c1d8b7b298b4fba2857b25a4
Matthew Evans <me388@cam.ac.uk>
2019-12-20 13:02:00 +0000

    Minor html validation fixes

--- odbx/odbx_routers/home.py
+++ odbx/odbx_routers/home.py
@@ -6,11 +6,15 @@ from ..odbx_templates import TEMPLATES
 
 
 async def homepage(request):
+
+    example_queries = ['elements HAS "Na"', 'chemical_formula_reduced="ClNa"']
+
     context = {
         "request": request,
         "odbx_title": "odbx",
         "odbx_blurb": "the open database of xtals",
         "odbx_about": 'odbx is a public database of crystal structures from the group of <a href="https://ajm143.github.io">Dr Andrew Morris</a> at the University of Birmingham, currently in public beta. Please contact <a href="mailto:web@odbx.science">web@odbx.science</a> with any issues.',
+        "example_queries": example_queries,
     }
     return TEMPLATES.TemplateResponse("search.html", context)
 

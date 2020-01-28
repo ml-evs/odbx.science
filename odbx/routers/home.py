39263f7b3f4fcb883e9fc3292d1d68de7c01d157
Matthew Evans <me388@cam.ac.uk>
2020-01-28 16:56:11 +0000

    Fixed typo

--- odbx/routers/home.py
+++ odbx/routers/home.py
@@ -9,7 +9,7 @@ async def homepage(request):
 
     example_queries = ['elements HAS "Na"', 'chemical_formula_reduced="ClNa"']
 
-    about_txt = 'odbx is a public database of crystal structures from the group of <a href="https://ajm143.github.io">Dr Andrew Morris</a> at the University of Birmingham, currently in public beta with a limited set of structures. Please contact <a href="mailto:web@odbx.science">web@odbx.science</a> with any issues.',
+    about_txt = 'odbx is a public database of crystal structures from the group of <a href="https://ajm143.github.io">Dr Andrew Morris</a> at the University of Birmingham, currently in public beta with a limited set of structures. Please contact <a href="mailto:web@odbx.science">web@odbx.science</a> with any issues.'
 
     context = {
         "request": request,

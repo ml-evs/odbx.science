ceb96f0f5e3dab7a22403f5f25a8e490d9c259e2
Matthew Evans <me388@cam.ac.uk>
2020-02-14 14:56:59 +0000

    Subclassed MongoCollection to use own MongoClient

---
+++ odbx/entry_collections.py
@@ -0,0 +1,26 @@
+import pymongo
+from optimade.server.entry_collections import MongoCollection
+from optimade.server.config import CONFIG
+
+CLIENT = pymongo.MongoClient(
+    CONFIG.mongo_uri, connectTimeoutMS=5000, serverSelectionTimeoutMS=5000
+)
+
+
+class OdbxMongoCollection(MongoCollection):
+    """ Wrap the default optimade collection to prevent Mongo errors from
+    directly being displayed to user.
+
+    """
+
+    def find(self, *args):
+        try:
+            return super().find(*args)
+        except (
+            pymongo.errors.ServerSelectionTimeoutError,
+            pymongo.errors.ConnectionFailure,
+        ):
+            raise RuntimeError(
+                "Backend currently unavailable, please try again in a few minutes.\n"
+                f"Report further issues to {CONFIG.implementation['maintainer']['email']}."
+            )

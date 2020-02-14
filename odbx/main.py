1b49f43e357980d1a4f93f12afe34bbb37f15059
Matthew Evans <me388@cam.ac.uk>
2020-02-14 14:58:36 +0000

    Updating main app to latest python-tools version

--- odbx/main.py
+++ odbx/main.py
@@ -7,6 +7,7 @@ implementation.
 import json
 from pathlib import Path
 
+from lark.exceptions import VisitError
 from pydantic import ValidationError
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
@@ -14,29 +15,23 @@ from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from starlette.staticfiles import StaticFiles  # odbx
 
-from optimade.server.entry_collections import MongoCollection
-from optimade.server.config import CONFIG
+from optimade import __api_version__
 from optimade.server.routers import info, links, references, structures, landing
 from optimade.server.middleware import RedirectSlashedURLs
-from optimade.server.routers.utils import get_providers, BASE_URL_PREFIXES
+from optimade.server.routers.utils import BASE_URL_PREFIXES
 
 import optimade.server.exception_handlers as exc_handlers
 
 from . import routers  # odbx
-
-config_version = "0.10.0"
+from .routers import ABOUT
 
 app = FastAPI(
-    title="OPTiMaDe API",
-    description=(
-        "The [Open Databases Integration for Materials Design (OPTiMaDe) consortium]"
-        "(http://http://www.optimade.org/) aims to make materials databases interoperational "
-        "by developing a common REST API."
-    ),
-    version=config_version,
-    docs_url="/optimade/extensions/docs",
-    redoc_url="/optimade/extensions/redoc",
-    openapi_url="/optimade/extensions/openapi.json",
+    title=ABOUT["title"],
+    description=ABOUT["about"],
+    version=__api_version__,
+    docs_url=f"{BASE_URL_PREFIXES['major']}/extensions/docs",
+    redoc_url=f"{BASE_URL_PREFIXES['major']}/extensions/redoc",
+    openapi_url=f"{BASE_URL_PREFIXES['major']}/extensions/openapi.json",
 )
 
 app.add_middleware(RedirectSlashedURLs)
@@ -46,12 +41,20 @@ app.add_exception_handler(
     RequestValidationError, exc_handlers.request_validation_exception_handler
 )
 app.add_exception_handler(ValidationError, exc_handlers.validation_exception_handler)
+app.add_exception_handler(VisitError, exc_handlers.grammar_not_implemented_handler)
 app.add_exception_handler(Exception, exc_handlers.general_exception_handler)
 
 app.include_router(info.router, prefix=BASE_URL_PREFIXES["major"])
 app.include_router(links.router, prefix=BASE_URL_PREFIXES["major"])
 app.include_router(references.router, prefix=BASE_URL_PREFIXES["major"])
 app.include_router(structures.router, prefix=BASE_URL_PREFIXES["major"])
+
+for version in ("minor", "patch"):
+    app.include_router(info.router, prefix=BASE_URL_PREFIXES[version])
+    app.include_router(links.router, prefix=BASE_URL_PREFIXES[version])
+    app.include_router(references.router, prefix=BASE_URL_PREFIXES[version])
+    app.include_router(structures.router, prefix=BASE_URL_PREFIXES[version])
+    app.include_router(landing.router, prefix=BASE_URL_PREFIXES[version])
 
 # add landing page
 app.include_router(landing.router, prefix="/optimade")
@@ -65,14 +68,3 @@ js_dir = Path(__file__).parent.joinpath("js")  # odbx
 css_dir = Path(__file__).parent.joinpath("css")  # odbx
 app.mount("/js", StaticFiles(directory=js_dir), name="js")  # odbx
 app.mount("/css", StaticFiles(directory=css_dir), name="css")  # odbx
-
-
-def update_schema(app):
-    """Update OpenAPI schema in file 'local_openapi.json'"""
-    with open("local_openapi.json", "w") as f:
-        json.dump(app.openapi(), f, indent=2)
-
-
-@app.on_event("startup")
-async def startup_event():
-    update_schema(app)

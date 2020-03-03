dc7fbd797a23887cd02e5afcc4d146765abdb136
Matthew Evans <me388@cam.ac.uk>
2020-03-03 17:37:52 +0000

    Moved chemdoodle to static folder and added task to pull latest version

--- tasks.py
+++ tasks.py
@@ -6,7 +6,7 @@ from invoke import task
 @task
 def get_js_libs(c):
     old_dir = os.getcwd()
-    lib_path = Path(__file__).parent.joinpath("odbx/js/lib")
+    lib_path = Path(__file__).parent.joinpath("odbx/static/js/lib")
     os.makedirs(lib_path, exist_ok=True)
     os.chdir(lib_path)
     c.run("wget https://web.chemdoodle.com/downloads/ChemDoodleWeb-8.0.0.zip")

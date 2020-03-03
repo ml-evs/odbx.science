ed8b9e66e13686e0e46291030d842e56b709d697
Matthew Evans <me388@cam.ac.uk>
2020-03-03 17:39:59 +0000

    Tweaked production startup script

--- scripts/start_production.sh
+++ scripts/start_production.sh
@@ -4,6 +4,8 @@ mkdir -p /tmp
 gunicorn \
     -w 1 \
     -k uvicorn.workers.UvicornWorker \
-    --error-logfile logs/error.log \
-    --access-logfile logs/access.log \
+    --error-logfile logs/odbx_error.log \
+    --access-logfile logs/odbx_access.log \
+    --capture-output \
+    --access-logformat "%(t)s: %(h)s %(l)s %(u)s %(r)s %(s)s %(b)s %(f)s %(a)s" \
     -b unix:/tmp/gunicorn.sock odbx.main:app;

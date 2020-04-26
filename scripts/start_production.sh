01dcc587fceb561345ebf3f73a1792ce94c39f28
Matthew Evans <me388@cam.ac.uk>
2020-04-26 13:45:46 +0000

    Tail logs when running servers

--- scripts/start_production.sh
+++ scripts/start_production.sh
@@ -2,10 +2,12 @@
 mkdir -p logs
 mkdir -p /tmp
 gunicorn \
-    -w 1 \
+    -w 2 \
     -k uvicorn.workers.UvicornWorker \
     --error-logfile logs/odbx_error.log \
     --access-logfile logs/odbx_access.log \
     --capture-output \
     --access-logformat "%(t)s: %(h)s %(l)s %(u)s %(r)s %(s)s %(b)s %(f)s %(a)s" \
-    -b unix:/tmp/gunicorn.sock odbx.main:app;
+    -b unix:/tmp/gunicorn.sock odbx.main:app &
+
+tail -f -n 20 logs/odbx_access.log logs/odbx_error.log
--- scripts/start_production_rest.sh
+++ scripts/start_production_rest.sh
@@ -2,10 +2,12 @@
 mkdir -p logs
 mkdir -p /tmp
 gunicorn \
-    -w 1 \
+    -w 2 \
     -k uvicorn.workers.UvicornWorker \
     --error-logfile logs/odbx_rest_error.log \
     --access-logfile logs/odbx_rest_access.log \
     --capture-output \
     --access-logformat "%(t)s: %(h)s %(l)s %(u)s %(r)s %(s)s %(b)s %(f)s %(a)s" \
-    -b unix:/tmp/gunicorn_rest.sock odbx.main_rest:app;
+    -b unix:/tmp/gunicorn_rest.sock odbx.main_rest:app &
+
+tail -f -n 20 logs/odbx_rest_access.log logs/odbx_rest_error.log

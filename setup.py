01ecc1e79b2bfec2364228afd42d8c5a8af8c229
Matthew Evans <me388@cam.ac.uk>
2020-05-26 18:01:25 +0100

    Handle git dependency links

--- setup.py
+++ setup.py
@@ -3,6 +3,14 @@ from setuptools import setup
 
 module_dir = Path(__file__).resolve().parent
 
+with open("requirements.txt", "r") as f:
+    reqs = f.readlines()
+
+dependency_links = [req for req in reqs if req.startswith('-e git://')]
+for ind, req in enumerate(reqs):
+    if req in dependency_links:
+        reqs[ind] = req.split('#egg=')[-1]
+
 setup(
     name="odbx",
     version="0.4.0",
@@ -11,6 +19,8 @@ setup(
     author="Matthew Evans",
     author_email="web@odbx.science",
     include_package_data=True,
+    install_requires=reqs,
+    dependency_links=dependency_links,
     packages=["odbx"],
     python_requires=">=3.6",
 )

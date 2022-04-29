a97362af092264ecec5c1239fff88f4a669f6138
Matthew Evans <git@ml-evs.science>
2022-04-29 16:57:01 +0000

    Update certbot script to use docker compose

--- scripts/certbot.sh
+++ scripts/certbot.sh
@@ -1,1 +1,1 @@
-docker-compose run --rm --entrypoint "certbot certonly --webroot -w /var/www/certbot --email admin@odbx.science --agree-tos --no-eff-email -d www.odbx.science -d optimade-index.odbx.science -d optimade.odbx.science -d odbx.science -d datalab.odbx.science -d api.odbx.science" certbot
+docker compose run --rm --entrypoint "certbot certonly --webroot -w /var/www/certbot --email admin@odbx.science --agree-tos --no-eff-email -d www.odbx.science -d optimade-index.odbx.science -d optimade.odbx.science -d odbx.science -d datalab.odbx.science -d api.odbx.science" certbot
